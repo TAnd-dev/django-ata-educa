d71b65c1ec4926e93f165b3760d67f4dfb30120e
Andrei <titov680andrei@gmail.com>
2024-08-27 21:37:53 +0400

    Added the ability to view, add, update, delete courses

--- base/urls.py
+++ base/urls.py
@@ -16,11 +16,15 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_view
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('accounts/login/', auth_view.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_view.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls'))
 ]
 
 if settings.DEBUG:
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,45 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse_lazy
+from django.views import generic
 from django.shortcuts import render
 
-# Create your views here.
+from courses.models import Course
+
+
+class OwnerMixin:
+    def get_queryset(self):
+        return super().get_queryset().filter(owner=self.request.user)
+
+
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    template_name = 'courses/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseMixin, generic.ListView):
+    template_name = 'courses/manage/course/list.html'
+    permission_required = 'courses.view_course'
+
+
+class CourseCreateView(OwnerCourseEditMixin, generic.CreateView):
+    permission_required = 'courses.add_course'
+
+
+class CourseUpdateView(OwnerCourseEditMixin, generic.UpdateView):
+    permission_required = 'courses.change_course'
+
+
+class CourseDeleteView(OwnerCourseMixin, generic.DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+    permission_required = 'courses.delete_course'
