1279bd839bbf3ed8783098cda811f1b4a1ae4966
Andrei <titov680andrei@gmail.com>
2024-08-29 19:56:01 +0400

    Added the ability to view courses you are subscribed to and view their content

--- base/settings.py
+++ base/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'embed_video',
 
     'students',
 ]
@@ -122,3 +126,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('student_course_list')
--- students/views.py
+++ students/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.shortcuts import render
 
+from courses.models import Course
 from students.forms import CourseEnrollForm
 
 
@@ -34,3 +35,31 @@ class StudentEnrollCourseView(LoginRequiredMixin, generic.edit.FormView):
 
     def get_success_url(self):
         return reverse_lazy('student_course_detail', args=[self.course.id])
+
+
+class StudentCourseListView(LoginRequiredMixin, generic.ListView):
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+
+class StudentCourseDetailView(LoginRequiredMixin, generic.DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        course = self.get_object()
+        if 'module_id' in self.kwargs:
+            context['module'] = course.modules.get(id=self.kwargs['module_id'])
+        else:
+            context['module'] = course.modules.all()[0]
+        return context
